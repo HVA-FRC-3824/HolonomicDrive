90cf88c44503201a4b161e265a01f1c0effa14c3
Robert Palmer Jr <robert.palmer@ingenutec.com>
2015-01-31 11:39:57 -0500

    Adding tasks

---
+++ src/org/usfirst/frc3824/HolonomicDrive/Tasks.java
@@ -0,0 +1,8 @@
+package src.org.usfirst.frc3824.HolonomicDrive;
+
+public class Tasks {
+	// TODO: Get Drive Camera working
+	// TODO: Rotate robot about center of totes
+	// TODO: Acceleration/Deceleration profile on lift
+	// TODO: Gripper arm actuation
+}
