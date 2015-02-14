2be5801c24a58cda0a2d995c7ca8874678511aef
Spencer Gehin <scGehin@gmail.com>
2015-02-14 10:50:10 -0500

    Fixed AutonomousDragToteRight.java 	I forgot that we can't be touching the tote at the end of 	autonomous, and also forgot a (.

--- src/org/usfirst/frc3824/HolonomicDrive/commands/AutonomousDragToteRight.java
+++ src/org/usfirst/frc3824/HolonomicDrive/commands/AutonomousDragToteRight.java
@@ -5,6 +5,8 @@ public class AutonomousDragToteRight.java extends CommandGroup
 {
 	public AutonomousDragToteRight(){
     	addParallel(new ForkliftMoveToPosition(Constants.FORKLIFT_TOTE1_POSITION));
-	addSequential(new ChassisDriveStraight(2.5, 0.5, -90);
+	addSequential(new ChassisDriveStraight(2.5, 0.5, -90));
+	addSequential(new ForkliftMoveToPosition(Constants.FORKLIFT_TOTEPICKUP_POSITION));
+	addSequential(new ChassisDriveStraight(1.0, 0.5, 180));
 	}
 }
