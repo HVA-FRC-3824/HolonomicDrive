147613ddb9e931775ab98566bf12b8b48efe64a4
Spencer Gehin <scGehin@gmail.com>
2015-02-14 10:32:52 -0500

    Created AutonomousDragToteRight 	Starting with the robot touching the tote, raises the tote up 	and strafes right into the autozone.

---
+++ src/org/usfirst/frc3824/HolonomicDrive/commands/AutonomousDragToteRight.java
@@ -0,0 +1,10 @@
+package org.usfirst.frc3824.HolonomicDrive.commands;
+import org.usfirst.frc3824.HolonomicDrive.Constants;
+import edu.wpi.first.wpilibj.command.CommandGroup;
+public class AutonomousDragToteRight.java extends CommandGroup
+{
+	public AutonomousDragToteRight(){
+    	addParallel(new ForkliftMoveToPosition(Constants.FORKLIFT_TOTE1_POSITION));
+	addSequential(new ChassisDriveStraight(2.5, 0.5, -90);
+	}
+}
