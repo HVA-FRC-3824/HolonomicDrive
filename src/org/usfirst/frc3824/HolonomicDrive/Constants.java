293aab5c90cc58fb427f643a62de7976ba471174
Robert Palmer Jr <robert.palmer@ingenutec.com>
2015-03-12 19:24:22 -0400

    Modified Constants class to allow for two sets of parameters - one for competition robot and one for practice robot.  It's defined at the top of the Constants class

--- src/org/usfirst/frc3824/HolonomicDrive/Constants.java
+++ src/org/usfirst/frc3824/HolonomicDrive/Constants.java
@@ -1,42 +1,182 @@
 package org.usfirst.frc3824.HolonomicDrive;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 public class Constants
 {
+	static final boolean IS_COMP_ROBOT = false;
+	
+	public static void InitConstants()
+	{
+		if(IS_COMP_ROBOT)
+		{
+			SmartDashboard.putString("Active Robot Values", "COMPETITION ROBOT");
+		    FORKLIFT_TOTEPICKUP_POSITION                  = COMP_FORKLIFT_TOTEPICKUP_POSITION;
+		    FORKLIFT_TOTE0_POSITION                       = COMP_FORKLIFT_TOTE0_POSITION;
+		    FORKLIFT_TOTE1_POSITION                       = COMP_FORKLIFT_TOTE1_POSITION;
+		    FORKLIFT_TOTE2_POSITION                       = COMP_FORKLIFT_TOTE2_POSITION;
+		    FORKLIFT_TOTE3_POSITION                       = COMP_FORKLIFT_TOTE3_POSITION;
+		    FORKLIFT_TOTE4_POSITION                       = COMP_FORKLIFT_TOTE4_POSITION;
+		    FORKLIFT_TOTE5_POSITION                       = COMP_FORKLIFT_TOTE5_POSITION;
+		    
+		    FORKLIFT_STEP                                 = COMP_FORKLIFT_STEP;
+		    FORKLIFT_CONTAINER_PICKUP                     = COMP_FORKLIFT_CONTAINER_PICKUP;
+		    FORKLIFT_CONTAINER_LIFT                       = COMP_FORKLIFT_CONTAINER_LIFT;
+		    
+		    FORKLIFT_TOTE_LOAD_HEIGHT                     = COMP_FORKLIFT_TOTE_LOAD_HEIGHT;
+		    	
+		    //***************************************************************************************
+		    // Drive train turn constants
+		    DRIVETRAIN_DRIVE_STRAIGHT_P                   = COMP_DRIVETRAIN_DRIVE_STRAIGHT_P;
+		    DRIVETRAIN_DRIVE_STRAIGHT_I                   = COMP_DRIVETRAIN_DRIVE_STRAIGHT_I;
+		    DRIVETRAIN_DRIVE_STRAIGHT_D                   = COMP_DRIVETRAIN_DRIVE_STRAIGHT_D;
+	
+		    DRIVETRAIN_TURN_ANGLE_P                       = COMP_DRIVETRAIN_TURN_ANGLE_P;
+		    DRIVETRAIN_TURN_ANGLE_I                       = COMP_DRIVETRAIN_TURN_ANGLE_I;
+		    DRIVETRAIN_TURN_ANGLE_D                       = COMP_DRIVETRAIN_TURN_ANGLE_D;
+		    
+		    DRIVETRAIN_DRIVE_AND_TURN_P                   = COMP_DRIVETRAIN_DRIVE_AND_TURN_P;
+		    DRIVETRAIN_DRIVE_AND_TURN_I                   = COMP_DRIVETRAIN_DRIVE_AND_TURN_I;
+		    DRIVETRAIN_DRIVE_AND_TURN_D                   = COMP_DRIVETRAIN_DRIVE_AND_TURN_D;
+		}
+		else // ---------- PRACTICE ROBOT ----------
+		{
+			SmartDashboard.putString("Active Robot Values", "PRACTICE ROBOT");
+		    FORKLIFT_TOTEPICKUP_POSITION                  = PRACTICE_FORKLIFT_TOTEPICKUP_POSITION;
+		    FORKLIFT_TOTE0_POSITION                       = PRACTICE_FORKLIFT_TOTE0_POSITION;
+		    FORKLIFT_TOTE1_POSITION                       = PRACTICE_FORKLIFT_TOTE1_POSITION;
+		    FORKLIFT_TOTE2_POSITION                       = PRACTICE_FORKLIFT_TOTE2_POSITION;
+		    FORKLIFT_TOTE3_POSITION                       = PRACTICE_FORKLIFT_TOTE3_POSITION;
+		    FORKLIFT_TOTE4_POSITION                       = PRACTICE_FORKLIFT_TOTE4_POSITION;
+		    FORKLIFT_TOTE5_POSITION                       = PRACTICE_FORKLIFT_TOTE5_POSITION;
+		    
+		    FORKLIFT_STEP                                 = PRACTICE_FORKLIFT_STEP;
+		    FORKLIFT_CONTAINER_PICKUP                     = PRACTICE_FORKLIFT_CONTAINER_PICKUP;
+		    FORKLIFT_CONTAINER_LIFT                       = PRACTICE_FORKLIFT_CONTAINER_LIFT;
+		    
+		    FORKLIFT_TOTE_LOAD_HEIGHT                     = PRACTICE_FORKLIFT_TOTE_LOAD_HEIGHT;
+		    	
+		    //***************************************************************************************
+		    // Drive train turn constants
+		    DRIVETRAIN_DRIVE_STRAIGHT_P                   = PRACTICE_DRIVETRAIN_DRIVE_STRAIGHT_P;
+		    DRIVETRAIN_DRIVE_STRAIGHT_I                   = PRACTICE_DRIVETRAIN_DRIVE_STRAIGHT_I;
+		    DRIVETRAIN_DRIVE_STRAIGHT_D                   = PRACTICE_DRIVETRAIN_DRIVE_STRAIGHT_D;
+	
+		    DRIVETRAIN_TURN_ANGLE_P                       = PRACTICE_DRIVETRAIN_TURN_ANGLE_P;
+		    DRIVETRAIN_TURN_ANGLE_I                       = PRACTICE_DRIVETRAIN_TURN_ANGLE_I;
+		    DRIVETRAIN_TURN_ANGLE_D                       = PRACTICE_DRIVETRAIN_TURN_ANGLE_D;
+		    
+		    DRIVETRAIN_DRIVE_AND_TURN_P                   = PRACTICE_DRIVETRAIN_DRIVE_AND_TURN_P;
+		    DRIVETRAIN_DRIVE_AND_TURN_I                   = PRACTICE_DRIVETRAIN_DRIVE_AND_TURN_I;
+		    DRIVETRAIN_DRIVE_AND_TURN_D                   = PRACTICE_DRIVETRAIN_DRIVE_AND_TURN_D;			
+		}
+
+	}
     //***************************************************************************************
 	// gyro center value
 	public final static int GYRO_CENTER                                      = 1975175;
 	
     //***************************************************************************************
-    // forklift positions for each tote height
-	                                                                                 //  Competition Robot settings
-    public final static double FORKLIFT_TOTEPICKUP_POSITION                  =  0.0; //  0.0;
-    public final static double FORKLIFT_TOTE0_POSITION                       =  4.0; //  5.0;  //going to be well over the height of one tote
-    public final static double FORKLIFT_TOTE1_POSITION                       = 12.0; // 18.5;
-    public final static double FORKLIFT_TOTE2_POSITION                       = 22.0; // 30.0;
-    public final static double FORKLIFT_TOTE3_POSITION                       = 28.75; // 40.0;
-    public final static double FORKLIFT_TOTE4_POSITION                       = 38.0; // 49.0;
-    public final static double FORKLIFT_TOTE5_POSITION                       = 55.5; // 55.5;
+    //**************** Values that differ between practice and competition ******************
+    //***************************************************************************************
+   
+    public static double FORKLIFT_TOTEPICKUP_POSITION;
+    public static double FORKLIFT_TOTE0_POSITION;  //going to be well over the height of one tote
+    public static double FORKLIFT_TOTE1_POSITION;
+    public static double FORKLIFT_TOTE2_POSITION;
+    public static double FORKLIFT_TOTE3_POSITION;
+    public static double FORKLIFT_TOTE4_POSITION;
+    public static double FORKLIFT_TOTE5_POSITION;
     
-    public final static double FORKLIFT_STEP                                 = 10.5; // 10.5;
-    public final static double FORKLIFT_CONTAINER_PICKUP                     =  6.6; //  6.6;
-    public final static double FORKLIFT_CONTAINER_LIFT                       = 21.0; // 21.0;
+    public static double FORKLIFT_STEP;
+    public static double FORKLIFT_CONTAINER_PICKUP;
+    public static double FORKLIFT_CONTAINER_LIFT;
     
-    public final static double FORKLIFT_TOTE_LOAD_HEIGHT                     = 18.5; // 25.0;
+    public static double FORKLIFT_TOTE_LOAD_HEIGHT;
     	
     //***************************************************************************************
     // Drive train turn constants
-    public final static double DRIVETRAIN_DRIVE_STRAIGHT_P                   = 0.040;
-    public final static double DRIVETRAIN_DRIVE_STRAIGHT_I                   = 0.004;
-    public final static double DRIVETRAIN_DRIVE_STRAIGHT_D                   = 0.040;
+    public static double DRIVETRAIN_DRIVE_STRAIGHT_P;
+    public static double DRIVETRAIN_DRIVE_STRAIGHT_I;
+    public static double DRIVETRAIN_DRIVE_STRAIGHT_D;
 
-    public final static double DRIVETRAIN_TURN_ANGLE_P                       = 0.005;   // 0.005
-    public final static double DRIVETRAIN_TURN_ANGLE_I                       = 0.0001;  // 0.0005
-    public final static double DRIVETRAIN_TURN_ANGLE_D                       = 0.005;
+    public static double DRIVETRAIN_TURN_ANGLE_P;
+    public static double DRIVETRAIN_TURN_ANGLE_I;
+    public static double DRIVETRAIN_TURN_ANGLE_D;
     
-    public final static double DRIVETRAIN_DRIVE_AND_TURN_P                   = 0.005;
-    public final static double DRIVETRAIN_DRIVE_AND_TURN_I                   = 0.00001;
-    public final static double DRIVETRAIN_DRIVE_AND_TURN_D                   = 0.005;
+    public static double DRIVETRAIN_DRIVE_AND_TURN_P;
+    public static double DRIVETRAIN_DRIVE_AND_TURN_I;
+    public static double DRIVETRAIN_DRIVE_AND_TURN_D;
 
+    
+    //***************************************************************************************
+    //			                            ------- PRACTICE ROBOT SETTINGS ------
+    // forklift positions for each tote height
+	                                                                                 
+    public final static double PRACTICE_FORKLIFT_TOTEPICKUP_POSITION                  =  0.0;
+    public final static double PRACTICE_FORKLIFT_TOTE0_POSITION                       =  4.0;
+    public final static double PRACTICE_FORKLIFT_TOTE1_POSITION                       = 12.0;
+    public final static double PRACTICE_FORKLIFT_TOTE2_POSITION                       = 22.0;
+    public final static double PRACTICE_FORKLIFT_TOTE3_POSITION                       = 28.75;
+    public final static double PRACTICE_FORKLIFT_TOTE4_POSITION                       = 38.0;
+    public final static double PRACTICE_FORKLIFT_TOTE5_POSITION                       = 55.5;
+    
+    public final static double PRACTICE_FORKLIFT_STEP                                 = 10.5;
+    public final static double PRACTICE_FORKLIFT_CONTAINER_PICKUP                     =  6.6;
+    public final static double PRACTICE_FORKLIFT_CONTAINER_LIFT                       = 21.0;
+    
+    public final static double PRACTICE_FORKLIFT_TOTE_LOAD_HEIGHT                     = 18.5;
+    	
+    //***************************************************************************************
+    // Drive train turn constants
+    public final static double PRACTICE_DRIVETRAIN_DRIVE_STRAIGHT_P                   = 0.040;
+    public final static double PRACTICE_DRIVETRAIN_DRIVE_STRAIGHT_I                   = 0.004;
+    public final static double PRACTICE_DRIVETRAIN_DRIVE_STRAIGHT_D                   = 0.040;
+
+    public final static double PRACTICE_DRIVETRAIN_TURN_ANGLE_P                       = 0.005;
+    public final static double PRACTICE_DRIVETRAIN_TURN_ANGLE_I                       = 0.0001;
+    public final static double PRACTICE_DRIVETRAIN_TURN_ANGLE_D                       = 0.005;
+    
+    public final static double PRACTICE_DRIVETRAIN_DRIVE_AND_TURN_P                   = 0.005;
+    public final static double PRACTICE_DRIVETRAIN_DRIVE_AND_TURN_I                   = 0.00001;
+    public final static double PRACTICE_DRIVETRAIN_DRIVE_AND_TURN_D                   = 0.005;
+
+    //***************************************************************************************
+    //			                            ------- COMPETITION ROBOT SETTINGS ------
+    // forklift positions for each tote height
+	                                                                                 
+    public final static double COMP_FORKLIFT_TOTEPICKUP_POSITION                  =  0.0;
+    public final static double COMP_FORKLIFT_TOTE0_POSITION                       =  5.0;  //going to be well over the height of one tote
+    public final static double COMP_FORKLIFT_TOTE1_POSITION                       = 18.5;
+    public final static double COMP_FORKLIFT_TOTE2_POSITION                       = 30.0;
+    public final static double COMP_FORKLIFT_TOTE3_POSITION                       = 40.0;
+    public final static double COMP_FORKLIFT_TOTE4_POSITION                       = 49.0;
+    public final static double COMP_FORKLIFT_TOTE5_POSITION                       = 55.5;
+    
+    public final static double COMP_FORKLIFT_STEP                                 = 10.5;
+    public final static double COMP_FORKLIFT_CONTAINER_PICKUP                     =  6.6;
+    public final static double COMP_FORKLIFT_CONTAINER_LIFT                       = 21.0;
+    
+    public final static double COMP_FORKLIFT_TOTE_LOAD_HEIGHT                     = 25.0;
+    	
+    //***************************************************************************************
+    // Drive train turn constants
+    public final static double COMP_DRIVETRAIN_DRIVE_STRAIGHT_P                   = 0.040;
+    public final static double COMP_DRIVETRAIN_DRIVE_STRAIGHT_I                   = 0.004;
+    public final static double COMP_DRIVETRAIN_DRIVE_STRAIGHT_D                   = 0.040;
+
+    public final static double COMP_DRIVETRAIN_TURN_ANGLE_P                       = 0.005;
+    public final static double COMP_DRIVETRAIN_TURN_ANGLE_I                       = 0.0005;
+    public final static double COMP_DRIVETRAIN_TURN_ANGLE_D                       = 0.005;
+    
+    public final static double COMP_DRIVETRAIN_DRIVE_AND_TURN_P                   = 0.005;
+    public final static double COMP_DRIVETRAIN_DRIVE_AND_TURN_I                   = 0.00001;
+    public final static double COMP_DRIVETRAIN_DRIVE_AND_TURN_D                   = 0.005;
+
+    //***************************************************************************************
+    //***************************************************************************************
+    //***************************************************************************************
+   
     public final static double TURN_THRESHOLD                                = 3.0; // works with 1, but is a bit slow
 
     //**************************************************************************************
